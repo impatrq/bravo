0adb730baa7a890914af266bed8a12d348c783aa
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-05 10:38:49 -0300

    espaciado

--- cliente/src/views/login.vue
+++ cliente/src/views/login.vue
@@ -1,11 +1,7 @@
 <template>
   <ion-page>
-    <!--ion-header>
-      <ion-toolbar>
-        <ion-title>Login</ion-title>
-      </ion-toolbar>
-    </ion-header-->
     <ion-content :fullscreen="true">
+
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Login</ion-title>
