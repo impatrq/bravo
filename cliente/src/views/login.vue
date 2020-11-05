a408871729cdcd9112f74e4f334fa2880123c640
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-05 12:07:48 -0300

    padding y tamaño de fuente

--- cliente/src/views/login.vue
+++ cliente/src/views/login.vue
@@ -29,14 +29,16 @@
               </div>
 
         <br>
-          <ion-item>
+
+              <ion-item class="ion-no-padding">
             <ion-label position="floating">Contraseña</ion-label>
             <ion-input type="password" id="contra" v-model="contra" @blur="validarcontra"></ion-input>
           </ion-item>
+              <div class="text-size-12">
             <span v-if="!contra">(La contraseña requiere un mínimo de una mayúscula y un número)</span>
             <span v-if="altocontra" color="red"> Formato incorrecto </span>
-      </ion-list>
-      <ion-button expand="block" >INICIAR SESIÓN</ion-button>
+              </div>
+
         <br>
           <ion-router-link to="olContraseña.vue">¿Olvidó su contraseña?</ion-router-link>
         <br>
