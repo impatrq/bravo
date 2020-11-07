6537c3f4a0dc9a5e6e499ab67a5a7ebe1746899a
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 17:28:28 -0300

    Lista de Componetes

---
+++ cliente/src/views/valEmail.vue
@@ -0,0 +1,18 @@
+      <ion-list>
+        <div class="ion-no-padding">
+          <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
+          <ion-label>Paso 2: Confirme su identidad</ion-label>
+        </div>
+        <ion-item class="ion-no-padding">
+          <ion-label>Bienvenido L</ion-label>
+          <ion-label>Su direccion de correo es un**irecc****cual******@gmail.com?</ion-label>
+        </ion-item> 
+        <br>
+        <ion-row class="ion-justify-content-center">
+        <ion-button size="small" color="success" href="#" fill="outline" >Si</ion-button>
+        <ion-button size="small" color="danger" href="#" fill="outline" >No</ion-button>            
+        </ion-row>   
+        <br>
+        <ion-router-link href="tabs/login" class="text-size-12">¿Ya tiene una cuenta? Inicie sesión aquí.</ion-router-link>            
+        
+      </ion-list>
