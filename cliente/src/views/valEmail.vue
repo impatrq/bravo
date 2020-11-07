f47164309199a866c00e60f1a41af956796ed8da
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 17:42:45 -0300

    Pregunta y Espacio entre etiqueta y botones

--- cliente/src/views/valEmail.vue
+++ cliente/src/views/valEmail.vue
@@ -21,12 +21,10 @@
           <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
           <ion-label>Paso 2: Confirme su identidad</ion-label>
         </div>
-        <ion-item class="ion-no-padding">
-          <ion-label>Bienvenido L</ion-label>
-          <ion-label>Su direccion de correo es un**irecc****cual******@gmail.com?</ion-label>
-        </ion-item> 
         <br>
-        <ion-row class="ion-justify-content-center">
+        <ion-label>Su direccion de correo es "al***ma**@gmail.com?</ion-label>
+        <br>
+        <ion-row class="ion-justify-content-center padding-top-20">
         <ion-button size="small" color="success" href="#" fill="outline" >Si</ion-button>
         <ion-button size="small" color="danger" href="#" fill="outline" >No</ion-button>            
         </ion-row>   
