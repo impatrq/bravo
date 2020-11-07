d7a09c19ed6010bd227a5b54769a2587913a6d9d
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 15:51:20 -0300

    base

---
+++ cliente/src/views/registroAlumno.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-page>
+    <!--ion-header>
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header-->
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Login</ion-title>
+        </ion-toolbar>
+      </ion-header>
