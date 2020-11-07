d3a459d187e745840579b4db7895dee6b81bfcd3
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 15:52:18 -0300

    fila, colummna y carta (inicio)

--- cliente/src/views/registroAlumno.vue
+++ cliente/src/views/registroAlumno.vue
@@ -11,3 +11,8 @@
           <ion-title size="large">Login</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <ion-row class="ion-justify-content-center fullheight">
+      <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
+      <ion-card class="ion-padding">
+
