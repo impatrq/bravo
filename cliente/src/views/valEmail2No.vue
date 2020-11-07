78745ef32a76a8db6c2ce42a42ae5e45412ec4ff
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 20:45:52 -0300

    Lista y Componentes

---
+++ cliente/src/views/valEmail2No.vue
@@ -0,0 +1,9 @@
+      <ion-list>
+        <div class="ion-no-padding">
+          <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
+          <ion-label>Paso 2: Confirme su identidad</ion-label>
+        </div>
+        <br>
+        <ion-label>Para registrar o corregir su Direccion de correo hable con su profesor o preceptor a cargo</ion-label>
+           
+      </ion-list>
