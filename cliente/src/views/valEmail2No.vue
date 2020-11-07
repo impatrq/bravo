6c8d0c59e9fa97cc7fa21052022b878c01aae970
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 20:46:43 -0300

    Fila, Columna y Carta

--- cliente/src/views/valEmail2No.vue
+++ cliente/src/views/valEmail2No.vue
@@ -1,3 +1,7 @@
+      <ion-row class="ion-justify-content-center fullheight">
+      <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
+      <ion-card class="ion-padding">
+
       <ion-list>
         <div class="ion-no-padding">
           <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
@@ -7,3 +11,7 @@
         <ion-label>Para registrar o corregir su Direccion de correo hable con su profesor o preceptor a cargo</ion-label>
            
       </ion-list>
+
+      </ion-card>
+      </ion-col>
+      </ion-row>       
