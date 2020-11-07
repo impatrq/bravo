5a95aecd1b2736b420fd590ea2a0e1ec9c34745b
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 16:44:21 -0300

    fila columna carta y lista

--- cliente/src/views/registroPanol.vue
+++ cliente/src/views/registroPanol.vue
@@ -11,3 +11,32 @@
           <ion-title size="large">Login</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <ion-row class="ion-justify-content-center fullheight">
+      <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
+      <ion-card class="ion-padding">
+
+      <ion-list>
+        <div class="ion-no-padding">
+          <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
+          <ion-label>Paso 1: Ingrese su D.N.I.</ion-label>
+        </div>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">D.N.I.</ion-label>
+          <ion-input></ion-input>
+        </ion-item> 
+        <br>
+        <ion-row class="ion-justify-content-center">
+        <ion-button size="small" href="tabs/cargosReg" fill="outline" >Atrás</ion-button>
+        <ion-button size="small" href="#" fill="outline" >Continuar</ion-button>           
+        </ion-row>
+        <br>        
+        <ion-router-link href="tabs/login" class="text-size-12">¿Ya tiene una cuenta? Inicie sesión aquí.</ion-router-link>
+      </ion-list>
+
+      </ion-card>
+      </ion-col>
+      </ion-row>       
+    </ion-content>
+  </ion-page>
+</template>
