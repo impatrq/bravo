f4ea85dcd4e9abc34d790314292b90bb5fe932a3
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 12:58:52 -0300

    Estilos Personalizados

--- cliente/src/views/registroCargo.vue
+++ cliente/src/views/registroCargo.vue
@@ -1,4 +1,16 @@
 
+<style>
+.text-size-12{
+  font-size:12px;
+}
+.text-size-20{
+  font-size:20px;
+}
+.fullheight{
+  height: 100%; 
+}
+</style>
+
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonListHeader, IonCol, IonRow } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
