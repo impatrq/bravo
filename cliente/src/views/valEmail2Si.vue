49e2bbe195a12d59f697ebdf9ea6860aa5b6f5aa
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 20:35:23 -0300

    Lista con los Componetes

---
+++ cliente/src/views/valEmail2Si.vue
@@ -0,0 +1,15 @@
+      <ion-list>
+        <div class="ion-no-padding">
+          <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
+          <ion-label>Paso 2: Confirme su identidad</ion-label>
+        </div>
+        <br>
+        <ion-label>Le enviamos un Email para que confirme su identidad</ion-label>
+        <br>
+        <ion-label>Si no recibio el Email, aprete "Reenviar Email"</ion-label>
+        <br>
+        <ion-row class="ion-justify-content-center padding-top-20">
+        <ion-button expand="block" fill="outline" href="#">Reenviar Email</ion-button>         
+        </ion-row>          
+        
+      </ion-list>
