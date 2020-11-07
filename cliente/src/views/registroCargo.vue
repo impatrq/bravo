e07d469da5dca9596e6ace33a86e1a8b522ae199
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 13:00:46 -0300

    Lista con titulo, botones y link

--- cliente/src/views/registroCargo.vue
+++ cliente/src/views/registroCargo.vue
@@ -1,3 +1,18 @@
+      <ion-list>
+        <div class="ion-no-padding">
+          <ion-list-header class="text-size-20 ion-text-justify">Seleccione su cargo</ion-list-header>
+        </div>
+        <br>
+        <ion-button href="#" expand="block" fill="outline">Preceptor o Directivo</ion-button>  
+        <br>    
+        <ion-button href="#" expand="block" fill="outline" >Profesor</ion-button>
+        <br>  
+        <ion-button href="#" expand="block" fill="outline" >Personal de pañol</ion-button>      
+        <br>
+        <ion-button href="#" expand="block" fill="outline" >Alumno</ion-button>
+        <br>        
+        <ion-router-link href="tabs/login" class="text-size-12">¿Ya tiene una cuenta? Inicie sesión aquí.</ion-router-link>
+      </ion-list>
 
 <style>
 .text-size-12{
