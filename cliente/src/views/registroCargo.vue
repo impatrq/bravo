b5cd1f3d3006275a014d0d018f7fe96ea880ba54
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-07 12:58:09 -0300

    Base de script

---
+++ cliente/src/views/registroCargo.vue
@@ -0,0 +1,12 @@
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonListHeader, IonCol, IonRow } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  {
+  name: 'registroCargo',
+  components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonListHeader, IonCol, IonRow },
+  methods: {
+  }
+}
+</script>
