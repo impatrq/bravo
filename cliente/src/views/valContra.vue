e7d744877720a82d1e26f666290f87d11f5e94c6
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 10:11:23 -0300

    Titulo de contraseña

---
+++ cliente/src/views/valContra.vue
@@ -0,0 +1,4 @@
+        <div class="ion-no-padding">
+          <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
+          <ion-label>Paso 3: Cree su contraseña</ion-label>
+        </div>
