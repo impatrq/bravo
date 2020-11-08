225175b4b90e4afc7061611eaba3e5bd1064cd69
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 10:17:50 -0300

    "Ion-list"

--- cliente/src/views/valContra.vue
+++ cliente/src/views/valContra.vue
@@ -1,3 +1,4 @@
+      <ion-list>
         <div class="ion-no-padding">
           <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
           <ion-label>Paso 3: Cree su contraseña</ion-label>
@@ -16,3 +17,4 @@
         <br>
         <ion-router-link href="tabs/login" class="text-size-12">¿Ya tiene una cuenta? Inicie sesión aquí.</ion-router-link>            
         
+      </ion-list>
