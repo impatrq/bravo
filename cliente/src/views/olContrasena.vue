d4f40218f53bd837d5fdcbc791e90a1cda8cf34d
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 10:06:34 -0300

    Saltos de linea e "Ion-input" de Email

--- cliente/src/views/olContrasena.vue
+++ cliente/src/views/olContrasena.vue
@@ -1,3 +1,10 @@
         <div class="ion-no-padding">
           <ion-list-header class="text-size-20 ion-no-padding">¿Olvidaste tu contraseña?</ion-list-header>
         </div>
+        <br>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Email</ion-label>
+          <ion-input></ion-input>
+        </ion-item>
+        <ion-label>Ejemplo: algún-mail-random@gmail.com</ion-label>
+        <br>
