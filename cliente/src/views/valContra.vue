efb854371ddd334f0c0f8f1056349ef7abda93e6
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 10:13:48 -0300

    Salto de linea e "Repita su contraseña"

--- cliente/src/views/valContra.vue
+++ cliente/src/views/valContra.vue
@@ -7,3 +7,8 @@
           <ion-label position="floating">Nueva contraseña</ion-label>
           <ion-input></ion-input>
         </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Repita su contraseña</ion-label>
+          <ion-input></ion-input>
+        </ion-item>
+        <br>
