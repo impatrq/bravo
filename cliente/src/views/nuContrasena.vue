1a9b2f3a295fc481ed7201077fdcc04880c99981
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 10:02:49 -0300

    Plantilla de la pagina

--- cliente/src/views/nuContrasena.vue
+++ cliente/src/views/nuContrasena.vue
@@ -1,3 +1,17 @@
+<template>
+  <ion-page>
+    <!--ion-header>
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header-->
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Login</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
       <ion-row class="ion-justify-content-center fullheight">
       <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
       <ion-card class="ion-padding">
@@ -23,3 +37,6 @@
       </ion-card>
       </ion-col>
       </ion-row>       
+    </ion-content>
+  </ion-page>
+</template>
