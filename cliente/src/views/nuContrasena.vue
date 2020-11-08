bea266bd55a79f8afefbf8df1052a4e21a501315
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 09:58:43 -0300

    "Ion-input" de "Repita su contraseña"

--- cliente/src/views/nuContrasena.vue
+++ cliente/src/views/nuContrasena.vue
@@ -2,3 +2,7 @@
           <ion-label position="floating">Nueva contraseña</ion-label>
           <ion-input></ion-input>
         </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Repita su contraseña</ion-label>
+          <ion-input></ion-input>
+        </ion-item>
