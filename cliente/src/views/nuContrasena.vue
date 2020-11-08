0f0d51118ecdb5b11ce766b0f3746255d89b68e5
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 10:00:13 -0300

    Titulo de la pagina

--- cliente/src/views/nuContrasena.vue
+++ cliente/src/views/nuContrasena.vue
@@ -1,3 +1,7 @@
+        <div class="ion-no-padding">
+          <ion-list-header class="text-size-20 ion-no-padding">Elija una nueva contraseña</ion-list-header>
+        </div>
+        <br>
         <ion-item class="ion-no-padding">
           <ion-label position="floating">Nueva contraseña</ion-label>
           <ion-input></ion-input>
