93a3920089fef4f3cb401ce4c42737599e09e4a7
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-08 10:12:25 -0300

    Salto de linea e "ion-input" de "Nueva contraseña"

--- cliente/src/views/valContra.vue
+++ cliente/src/views/valContra.vue
@@ -2,3 +2,8 @@
           <ion-list-header class="text-size-20 ion-no-padding">Registro</ion-list-header>
           <ion-label>Paso 3: Cree su contraseña</ion-label>
         </div>
+        <br>        
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Nueva contraseña</ion-label>
+          <ion-input></ion-input>
+        </ion-item>
