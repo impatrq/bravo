372ebca5ca09cde2b80e13a593bfc51bf6e0e2fe
LucasMRevel <lucasmreve@hotmail.com>
2020-11-09 17:26:14 -0300

    se importó vue y ref a valContra.vue

--- cliente/src/views/valContra.vue
+++ cliente/src/views/valContra.vue
@@ -62,6 +62,7 @@
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonListHeader, IonCol, IonRow } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { App, ref } from 'vue';
 
 export default  {
   name: 'valCont',
