69fa66622908b8abb96e688157c50ab49e114480
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-09 18:19:35 -0300

    Vincular home.vue a el boton tab

--- cliente/src/views/Tabs.vue
+++ cliente/src/views/Tabs.vue
@@ -2,9 +2,9 @@
   <ion-page>
     <ion-tabs>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
+        <ion-tab-button tab="Home" href="/tabs/home">
           <ion-icon :icon="home" />
-          <ion-label>Tab 1</ion-label>
+          <ion-label>Home</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
