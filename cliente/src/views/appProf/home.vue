b2bd42642c3ebcf0a2baf3f692ca96f7d467163b
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-12 09:50:06 -0300

    Borrado de espacios (2)

--- cliente/src/views/appProf/home.vue
+++ cliente/src/views/appProf/home.vue
@@ -5,18 +5,17 @@
   <ion-toolbar><img src="@/components/img/headerLogo.png" class="ion-padding"></ion-toolbar>
 </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Home</ion-title>
-        </ion-toolbar>
-      </ion-header>
+<ion-content :fullscreen="true">
+  <ion-header collapse="condense">
+    <ion-toolbar><ion-title size="large">Home</ion-title></ion-toolbar>
+  </ion-header>
 
-      <ion-row class="ion-justify-content-center fullheight">
-      <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
+  <ion-row class="ion-justify-content-center fullheight">
+  <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
 
-          <!--Novedades-->
+    <ion-slides pager="true" :options="slideOpts">  
           <ion-slides pager="true" :options="slideOpts">
+    <ion-slides pager="true" :options="slideOpts">  
 
             <!--novedad 1-->
             <ion-slide>
