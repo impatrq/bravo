4946a660f937ec5b1cb7d324c4fd1ca5efe3b139
LucasMRevel <lucasmreve@hotmail.com>
2020-11-12 14:01:35 -0300

    se implementaron los estilos

---
+++ cliente/src/views/appProf/maletin.vue
@@ -0,0 +1,19 @@
+<style>
+.margin-buttom-60{
+  margin-bottom: 60px;
+}
+.fullheight{
+  height: 100%; 
+}
+.width-95per{
+  width: 95%; 
+}
+.background-grey{
+  background-color: rgb(50, 50, 50);
+}
+.background-white{
+background-color: #808289 ;
+}
+
+</style>
+
