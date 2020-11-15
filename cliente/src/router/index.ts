3c3bd40f60d06b7f7ec209248466a5f7d8e62294
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-15 10:54:43 -0300

    deje en "tabs" las cosas no relacionadas al login

--- cliente/src/router/index.ts
+++ cliente/src/router/index.ts
@@ -16,6 +16,27 @@ const routes: Array<RouteRecordRaw> = [
         redirect: 'tab1'
       },
       {
+        path: 'home',
+        component: () => import('@/views/appProf/home.vue')
+      },
+      {
+        path: 'maletin',
+        component: () => import('@/views/appProf/maletin.vue')
+      },
+      {
+        path: 'tab1',
+        component: () => import('@/views/Tab1.vue')
+      },
+      {
+        path: 'tab2',
+        component: () => import('@/views/Tab2.vue')
+      },
+      {
+        path: 'tab3',
+        component: () => import('@/views/Tab3.vue')
+      }
+    ]
+  },
         path: 'login',
         component: () => import('@/views/login.vue')
       },
