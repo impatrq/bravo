8f7d6b823e4727524542b11182b59727cd757490
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-15 16:45:51 -0300

    Eliminacion de Coment., esp. y uso de routerLink

--- cliente/src/views/nuContrasena.vue
+++ cliente/src/views/nuContrasena.vue
@@ -1,38 +1,33 @@
 <template>
-  <ion-page>
-    <!--ion-header>
-      <ion-toolbar>
-        <ion-title>Login</ion-title>
-      </ion-toolbar>
-    </ion-header-->
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Login</ion-title>
-        </ion-toolbar>
-      </ion-header>
+<ion-page>
+<ion-content :fullscreen="true">
 
-      <ion-row class="ion-justify-content-center fullheight">
-      <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
-      <ion-card class="ion-padding">
+  <ion-header collapse="condense">
+    <ion-toolbar>
+      <ion-title size="large">Login</ion-title>
+    </ion-toolbar>
+  </ion-header>
 
-      <ion-list>
-        <div class="ion-no-padding">
-          <ion-list-header class="text-size-20 ion-no-padding">Elija una nueva contraseña</ion-list-header>
-        </div>
-        <br>
-        <ion-item class="ion-no-padding">
-          <ion-label position="floating">Nueva contraseña</ion-label>
-          <ion-input></ion-input>
-        </ion-item>
-        <ion-item class="ion-no-padding">
-          <ion-label position="floating">Repita su contraseña</ion-label>
-          <ion-input></ion-input>
-        </ion-item>
-        <br>
-        <ion-button expand="block" href="#" fill="outline" >Confirmar</ion-button>            
-        
-      </ion-list>
+  <ion-row class="ion-justify-content-center fullheight">
+  <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
+  <ion-card class="ion-padding">
+
+    <ion-list>
+      <div class="ion-no-padding">
+        <ion-list-header class="text-size-20 ion-no-padding">Elija una nueva contraseña</ion-list-header>
+      </div>
+      <br>
+      <ion-item class="ion-no-padding">
+        <ion-label position="floating">Nueva contraseña</ion-label>
+        <ion-input></ion-input>
+      </ion-item>
+      <ion-item class="ion-no-padding">
+        <ion-label position="floating">Repita su contraseña</ion-label>
+        <ion-input></ion-input>
+      </ion-item>
+      <br>
+      <ion-button expand="block" routerLink="login" fill="outline" >Confirmar</ion-button>
+    </ion-list>
 
       </ion-card>
       </ion-col>
