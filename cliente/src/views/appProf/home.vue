74f0aa27af268892eba78685573b1dacb632aaeb
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-16 01:08:10 -0300

    Borrado de componentes innecesarios

--- cliente/src/views/appProf/home.vue
+++ cliente/src/views/appProf/home.vue
@@ -6,9 +6,6 @@
 </ion-header>
 
 <ion-content :fullscreen="true">
-  <ion-header collapse="condense">
-    <ion-toolbar><ion-title size="large">Home</ion-title></ion-toolbar>
-  </ion-header>
 
   <ion-row class="ion-justify-content-center fullheight">
   <ion-col size-xl="4" size-sm="10" class="ion-align-self-center">
