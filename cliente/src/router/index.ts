495c6f20c443961dfc4758663ee31518272dbd13
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-16 11:54:53 -0300

    Mover "home" y "maletin" a  /

--- cliente/src/router/index.ts
+++ cliente/src/router/index.ts
@@ -14,15 +14,7 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: '',
         redirect: 'tab1'
-      },
-      {
-        path: 'home',
-        component: () => import('@/views/appProf/home.vue')
-      },
-      {
-        path: 'maletin',
-        component: () => import('@/views/appProf/maletin.vue')
-      },
+      },      
       {
         path: 'tab1',
         component: () => import('@/views/Tab1.vue')
