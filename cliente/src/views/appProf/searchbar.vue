f9f61f5832291829c1313e5a8d734ed18ec3a491
mirko56878 <73917238+mirko56878@users.noreply.github.com>
2020-11-19 18:54:04 -0300

    Barra de busqueda
    Se creo un searchbar, falta aplicar filtro por letras

---
+++ cliente/src/views/appProf/searchbar.vue
@@ -0,0 +1,38 @@
+<template>
+<ion-page>
+
+    <ion-header translucent>
+      <ion-toolbar>
+        <ion-searchbar></ion-searchbar>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content fullscreen>
+      <ion-list>
+        <ion-item>Profesor 1</ion-item>
+        <ion-item>Profesor 2</ion-item>
+        <ion-item>Profesor 3</ion-item>
+        <ion-item>Profesor 4</ion-item>
+        <ion-item>Profesor 5</ion-item>
+        <ion-item>Profesor 6</ion-item>
+      </ion-list>
+    </ion-content>
+
+    
+</ion-page>
+</template>
+
+<style>
+
+
+
+</style>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonContent, IonSearchbar} from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { IonPage, IonHeader, IonToolbar, IonContent, IonSearchbar},
+  
+});
+</script>
