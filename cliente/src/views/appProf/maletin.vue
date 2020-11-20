692dac016aed2a6a2273c4003d0b2362a1dca020
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-11-20 13:46:49 -0300

    nuevos estilos y eliminacion de antiguos

--- cliente/src/views/appProf/maletin.vue
+++ cliente/src/views/appProf/maletin.vue
@@ -153,34 +153,13 @@
 </template>
 
 <style>
-.margin-buttom-60{
-  margin-bottom: 60px;
-}
-.fullheight{
-  height: 100%; 
-}
-.width-95per{
-  width: 95%; 
-}
-.left-move{
-  padding-left: 250px;
-}
-.background-grey{
-  background-color: rgb(50, 50, 50);
-}
-.background-white{
-background-color: #808289 ;
-}
-.background-white2{
-  background-color: #92949c ;
-}
-.background-white3{
-  background-color: #383a3e ;
-}
-.background-white2{
-  background-color: #d7d8da ;
-}
-
+.fullheight{ height: 100%; }
+.background-grey{ background-color: rgb(50, 50, 50); }
+.icon{ color: white; font-size: 32px; }
+.list{ height: 350px; overflow: scroll; }
+.list-card{ width: 100%; background-color: transparent; }
+.no-deco{ text-decoration: none; color: white;}
+.margin-bottom-65px{ margin-bottom: 65px; }
 </style>
 
 <script lang="ts">
