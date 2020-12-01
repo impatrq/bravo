2aaa970f6e36d4af60efdf11c3d3d1530f718851
Altamiran0 <Altamiran0@users.noreply.github.com>
2020-12-01 20:33:16 -0300

    Nueva ruta para las vistas de profesores

--- cliente/src/router/index.ts
+++ cliente/src/router/index.ts
@@ -8,60 +8,20 @@ const routes: Array<RouteRecordRaw> = [
     redirect: '/login'
   },
   {
-    path: '/tabs/',
+    path: '/prof/',
     component: Tabs,
     children: [
       {
-        path: '',
-        redirect: 'tab1'
+        path: '/prof/',
+        redirect: 'home'
       },
       {
         path: 'home',
         component: () => import('@/views/appProf/home.vue')
       },
       {
-        path: 'searchbar',
-        component: () => import('@/views/appProf/searchbar.vue')
-      },
-      {
-        path: 'search',
-        component: () => import('@/views/appProf/search.vue')
-      },
-      {
         path: 'maletin',
         component: () => import('@/views/appProf/maletin.vue')
-      },
-      {
-        path: 'maletin2',
-        component: () => import('@/views/appAlum/maletin2.vue')
-      },
-      {
-        path: 'maletin3',
-        component: () => import('@/views/appPrecep/maletin3.vue')
-      },
-      /*{
-        path: 'tab1',
-        component: () => import('@/views/Tab1.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3.vue')
-      },*/
-      {
-        path: 'searchProfBas',
-        component: () => import('@/views/appProf/searchProfBas.vue')
-      },
-      {
-        path: 'searchProfAvio',
-        component: () => import('@/views/appProf/searchProfAvio.vue')
-      },
-      {
-        path: 'searchProfAero',
-        component: () => import('@/views/appProf/searchProfAero.vue')
       }
     ]
   },
