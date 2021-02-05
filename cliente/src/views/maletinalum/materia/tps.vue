ecd8c9a8c54fb17b18547b1660b45a883383882e
mirko56878 <73917238+mirko56878@users.noreply.github.com>
2021-02-05 17:43:43 -0300

    se creo vista de trabajos practicos

---
+++ cliente/src/views/maletinalum/materia/tps.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-page>
+    <ion-header translucent class="margin-buttom-40">
+      <ion-toolbar color="primary">
+        <router-link to="/mat1" slot="start"><ion-icon :icon="arrowBackOutline" class="icon"></ion-icon></router-link>
+        <ion-title color="light">Volver a "Materia"</ion-title>
+      </ion-toolbar>
+    </ion-header>
+            <ion-content :fullscreen="true">
+                <ion-row class="ion-justify-content-center fullheight">
+                    <ion-col size-xl="4" size-sm="10">
+                      
+                      </ion-col>
+                    </ion-row>
+            </ion-content>
+    </ion-page>
+</template>
+
+<style>
+.margin-buttom-60{
+  margin-bottom: 60px;
+}
+.fullheight{
+  height: 100%; 
+}
+.fullwidth{
+  width: 100%; 
+}
+.icon{ 
+   color: white; 
+   font-size: 25px;
+   margin-inline-start: 16px; 
+}
+</style>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonContent } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import{ arrowBackOutline }from 'ionicons/icons';
+
+export default defineComponent({
+  components: { IonPage, IonHeader, IonToolbar, IonContent },
+    setup() {
+
+    return { arrowBackOutline } 
+  }
+
+});
+</script>
