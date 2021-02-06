f2e84298f7efacca536858facb16ebdd6a1a64e9
mirko56878 <73917238+mirko56878@users.noreply.github.com>
2021-02-06 07:30:01 -0300

    se mejoro vista en "sProfBas"

--- cliente/src/views/search/searchProfBas.vue
+++ cliente/src/views/search/searchProfBas.vue
@@ -2,25 +2,78 @@
 <ion-page>
 
     <ion-header translucent>
-       <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-button href="/search">
-            Atrás
-          </ion-button>
-        </ion-buttons>
-
-        <ion-title>Ciclo Básico</ion-title>
-      </ion-toolbar>
+        <ion-toolbar color="primary">
+            <router-link to="/search" slot="start"><ion-icon :icon="arrowBackOutline" class="icon"></ion-icon></router-link>
+            <ion-title color="light">Ciclo Básico</ion-title>
+        </ion-toolbar>
     </ion-header>
     
     <ion-content fullscreen>
       <ion-list>
-        <ion-item>Profesor Bas 1</ion-item>
-        <ion-item>Profesor Bas 2</ion-item>
-        <ion-item>Profesor Bas 3</ion-item>
-        <ion-item>Profesor Bas 4</ion-item>
-        <ion-item>Profesor Bas 5</ion-item>
-        <ion-item>Profesor Bas 6</ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesor 1</h2>
+          </ion-label>
+        </ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher2.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesora 1</h2>
+          </ion-label>
+        </ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesor 2</h2>
+          </ion-label>
+        </ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher2.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesora 2</h2>
+          </ion-label>
+        </ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesor 3</h2>
+          </ion-label>
+        </ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher2.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesora 3</h2>
+          </ion-label>
+        </ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesor 4</h2>
+          </ion-label>
+        </ion-item>
+        <ion-item lines="full">
+          <ion-avatar slot="start">
+            <img src="@/components/img/teacher2.png">
+          </ion-avatar>
+          <ion-label>
+            <h2>Profesora 4</h2>
+          </ion-label>
+        </ion-item>
       </ion-list>
     </ion-content>
 
@@ -30,16 +83,24 @@
 
 <style>
 
-
+.icon{ 
+   color: white; 
+   font-size: 25px;
+   margin-inline-start: 16px; 
+}
 
 </style>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonContent} from '@ionic/vue';
 import { defineComponent } from 'vue';
+import{ arrowBackOutline }from 'ionicons/icons';
+
 
 export default defineComponent({
   components: { IonPage, IonHeader, IonToolbar, IonContent},
-  
+  setup() {
+    return { arrowBackOutline } 
+  }
 });
 </script>
