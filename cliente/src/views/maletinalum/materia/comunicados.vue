65a245674b03c1d81ca7a75f5630f605db3cd5a8
mirko56878 <73917238+mirko56878@users.noreply.github.com>
2021-02-07 20:25:47 -0300

    se creó vista de comunicados

--- cliente/src/views/maletinalum/materia/comunicados.vue
+++ cliente/src/views/maletinalum/materia/comunicados.vue
@@ -7,30 +7,96 @@
       </ion-toolbar>
     </ion-header>
             <ion-content :fullscreen="true">
-                <ion-row class="ion-justify-content-center fullheight">
-                    <ion-col size-xl="4" size-sm="10">
-                      
+                  <ion-row class="ion-justify-content-center fullheight">
+                      <ion-col size-xl="4" size-sm="10">
+                          <ion-card color="primary">
+                                    <ion-item lines="none" color="primary">
+                                      <ion-avatar slot="start">
+                                        <img src="@/components/img/teacher.png">
+                                      </ion-avatar>
+                                      <ion-label>
+                                        <h2>Profesor</h2>
+                                        <h4 class="gris">10 de mayo de 2021</h4>
+                                      </ion-label>
+                                    </ion-item>
+                                    <ion-card-content class="margin-buttom-70">
+                                      Hola a todos, adjunto trabajos prácticos y material, revisar sus pestañas correspondientes.
+                                    </ion-card-content>
+                                    <ion-item lines="full" color="primary" class="comentarios">
+                                      <ion-label>
+                                        <h4 class="gris">0 comentarios de la clase</h4>
+                                      </ion-label>
+                                    </ion-item>
+                         </ion-card>
+                         <ion-card color="primary">
+                                    <ion-item lines="none" color="primary">
+                                      <ion-avatar slot="start">
+                                        <img src="@/components/img/teacher.png">
+                                      </ion-avatar>
+                                      <ion-label>
+                                        <h2>Profesor</h2>
+                                        <h4 class="gris">Fecha</h4>
+                                      </ion-label>
+                                    </ion-item>
+                                    <ion-card-content class="margin-buttom-70">
+                                      Anuncio
+                                    </ion-card-content>
+                                    <ion-item lines="full" color="primary" class="comentarios">
+                                      <ion-label>
+                                        <h4 class="gris">0 comentarios de la clase</h4>
+                                      </ion-label>
+                                    </ion-item>
+                         </ion-card>
+                         <ion-card color="primary" class="margin-buttom-60">
+                                    <ion-item lines="none" color="primary">
+                                      <ion-avatar slot="start">
+                                        <img src="@/components/img/teacher.png">
+                                      </ion-avatar>
+                                      <ion-label>
+                                        <h2>Profesor</h2>
+                                        <h4 class="gris">Fecha</h4>
+                                      </ion-label>
+                                    </ion-item>
+                                    <ion-card-content class="margin-buttom-70">
+                                      Anuncio
+                                    </ion-card-content>
+                                    <ion-item lines="full" color="primary" class="comentarios">
+                                      <ion-label>
+                                        <h4 class="gris">0 comentarios de la clase</h4>
+                                      </ion-label>
+                                    </ion-item>
+                         </ion-card>
                       </ion-col>
-                    </ion-row>
+                  </ion-row>
             </ion-content>
     </ion-page>
 </template>
 
 <style>
+.margin-buttom-70{
+  max-block-size: 70px;
+  margin-bottom: 70px;
+}
 .margin-buttom-60{
   margin-bottom: 60px;
 }
 .fullheight{
   height: 100%; 
 }
-.fullwidth{
-  width: 100%; 
-}
 .icon{ 
-   color: white; 
-   font-size: 25px;
-   margin-inline-start: 16px; 
+  color: white; 
+  font-size: 25px;
+  margin-inline-end: 16px; 
 }
+.gris{
+  color: gray;
+}
+.comentarios{
+  border-top-style: solid;
+  border-color: gray;
+  border-width: unset;
+}
+
 </style>
 
 <script lang="ts">
