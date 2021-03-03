bc478246be794fd5c3e6ffe8833585fcf6e8bfed
lautyn2001 <lautyn2001@users.noreply.github.com>
2021-03-02 23:27:24 -0300

    Declaración de dependencias, establecemos conexión con base de datos, importamos las rutas e iniciamos servidor

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,36 @@
-const express = require("express");
-const nodemailer = require("nodemailer")
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const app = express();
+
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Conexión a Base de datos
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.9qthf.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+
+// importar routes
+const authRoutes = require('./Routes/auth');
+// route middlewares
+app.use('/api/user', authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funcionando'
+    })
+});
+
+// iniciar server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+})
